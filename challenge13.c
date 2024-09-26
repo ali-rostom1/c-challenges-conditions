4c0e90a8d322fd6ea461f4249532172d94477570
michael-rostom <ali.rostom220@gmail.com>
2024-09-26 12:46:21 +0100

    editing challenge 13

--- challenge13.c
+++ challenge13.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(){
-    float Ax,Bx,Ay,By;
-    float Px,Py;
-    printf("Entrez les coordonnées du premier point (Ax Ay): ");
-    while (scanf("%f %f", &Ax, &Ay) != 2) {
-        printf("Veuillez entrer des valeurs valides pour Ax et Ay: ");
+    float Xa,Xb,Ya,Yb;
+    float Xp,Yp;
+    printf("Entrez les coordonnées du premier point (Xa Ya): ");
+    while (scanf("%f %f", &Xa, &Ya) != 2) {
+        printf("Veuillez entrer des valeurs valides pour Xa et Ya: ");
         while (getchar() != '\n');
     }
     
-    printf("Entrez les coordonnées du deuxieme point (Bx By): ");
-    while (scanf("%f %f", &Bx, &By) != 2) {
-        printf("Veuillez entrer des valeurs valides pour Bx et By: ");
+    printf("Entrez les coordonnées du deuxieme point (Xb Yb): ");
+    while (scanf("%f %f", &Xb, &Yb) != 2) {
+        printf("Veuillez entrer des valeurs valides pour Xb et Yb: ");
         while (getchar() != '\n');
     }
-     printf("Entrez les coordonnées du troisieme point (Px Py): ");
-    while (scanf("%f %f", &Px, &Py) != 2) {
-        printf("Veuillez entrer des valeurs valides pour Px et Py: ");
+     printf("Entrez les coordonnées du troisieme point (Xp Yp): ");
+    while (scanf("%f %f", &Xp, &Yp) != 2) {
+        printf("Veuillez entrer des valeurs valides pour Xp et Yp: ");
         while (getchar() != '\n'); 
     }
-    float minX=Ax;
-    float mAxX=Bx;
-    float minY=Ay;
-    float mAxY=By;
-    if(Ax>Bx){
-        minX=Bx;
-        mAxX=Ax;
-    }
-    if(Ay>By){
-        minY=By;
-        mAxY=Ay;
-    }
-    if(Px<minX || Px>mAxX || Py<minY || Py>mAxY){
+    float minX=fmin(Xa,Xb);
+    float maxX=fmax(Xa,Xb);
+    float minY=fmin(Ya,Yb);
+    float maxY=fmax(Ya,Yb);
+    if(Xp<minX || Xp>maxX || Yp<minY || Yp>maxY){
         printf("le point n'appartient pas au segment");
         exit(0);
     }
-    if((Px-Ax)*(By-Ay)==(Py-Ay)*(Bx-Ax)){
+    float vectorABx=Xb-Xa;
+    float vectorABy=Yb-Ya;   // so now we have AB(x,y) and AP(x,y) and we can do ABx*APy == ABy*APx
+    float vectorAPx=Xp-Xa;
+    float vectorAPy=Yp-Ya;
+    if(vectorAPx*vectorABy==vectorAPy*vectorABx){
         printf("les trois points sont colineares donc votre points se trouve dans le segment.");
     }else printf("le point n'appartient pas au segment");
     return 0;
 }
-        
-
-
-    
